cb15ec14f624331ba29332ebfcffa747cc89c86a
sangunPark <sangunp703@naver.com>
2019-11-08 01:42:55 +0900

    오프닝 화면 완성

--- src/opening.js
+++ src/opening.js
@@ -1,6 +1,6 @@
 import React, {Component} from 'react'
+import {Redirect} from 'react-router-dom'
 import styles from './styles'
-import coral from '../resource/living_coral.jpg'
 
 const style = styles.opening
 
@@ -8,14 +8,60 @@ export default class Opening extends Component{
   constructor(props){
     super(props)
     this.state = {
-      jump: ''
+      jump: '', hold: false
     }
   }
+  mouseDrag(e){
+    if(this.state.hold){
+      const control = document.querySelector('.control')
+      const name = document.querySelector('.name')
+      const img_w = (e.clientX / window.outerWidth) * 100
+      const name_s = 12 * img_w * 0.01
+      const button = document.querySelector('.go')
+      const ask = document.querySelector('.ask')
+      
+      if(img_w >= 5 && img_w <= 90){
+        control.style.left = img_w + '%'
+        name.style.fontSize = name_s + 'vw'
+        ask.style.left = img_w + '%'
+      }
+      if(img_w >= 90){
+        button.style.display = 'block'
+      } else {
+        button.style.display = 'none'
+      }
+    }
+  }
+  mouseDown(e){
+    const ask = document.querySelector('.ask')
+    ask.style.display = 'none'
+    this.setState({hold: true})
+  }
+  mouseUp(e){
+    const ask = document.querySelector('.ask')
+    ask.style.display = 'block'
+    this.setState({hold: false})
+  }
+  next(e){
+    this.setState({jump: '/main'})
+  }
   render(){
+    if(this.state.jump){
+      return <Redirect to={this.state.jump} />
+    }
     return (
-      <div style={style.screen}>
-        <div style={style.case}><h1 style={style.name}>PARK SANG UN</h1></div>
-        <div style={style.cover}></div>
+      <div style={style.screen}
+        onMouseMove={e => this.mouseDrag(e)}
+        onMouseUp={e => this.mouseUp(e)}
+        onMouseLeave={e => this.mouseUp(e)}>
+        <h1 class="name" style={style.name}>PARK SANG UN</h1>
+        <p class="go" style={style.button}
+          onMouseOver={e => e.target.style.color="black"} 
+          onMouseOut={e => e.target.style.color = "white"}
+          onClick={e => this.next(e)}>Let's find out</p>
+        <p class="ask" style={style.ask}>Drag me!</p>
+        <img class="control" style={style.control}
+          onMouseDown={e => this.mouseDown(e)}/>
       </div>
     )
   }
--- src/styles.js
+++ src/styles.js
@@ -2,24 +2,52 @@ const styles = {
   opening: {
     screen:{
       position: 'fixed',
-      top: '0%',
-      left: '0%',
-      minWidth: '100%',
-      minHeight: '100%'
-    },
-    case: {
-      background: 'url(http://www.color-hex.com/palettes/26323.png)',
-      backgroundSize: '100%',
-      color: 'transparent',
-      WebkitBackgroundClip: 'text',
-      backgroundClip: 'text'
+      display: 'flex',
+      width: '100vw',
+      height: '100vh',
+      justifyContent: 'center',
+      alignItems: 'center'
     },
     name: {
+      background: 'url("./resource/colorful.jpg") repeat',
+      backgroundPosition: 'center',
+      backgroundSize: 'cover',
       textAlign: 'center',
-      textTransform:'uppercase',
-      fontSize: '10em',
-      fontWeight: '900'
-    }
+      backgroundClip: 'text',
+      WebkitBackgroundClip: 'text',
+      WebkitTextFillColor: 'transparent',
+      font: '900 0.5vw "Open Sans", sans-serif',
+      whiteSpace: 'nowrap',
+      cursor: 'default'
+    },
+    button: {
+      position: 'absolute',
+      bottom: '15%',
+      left: '50%',
+      transform: 'translateX(-50%)',
+      fontSize: '50px',
+      display: 'none',
+      cursor: 'pointer',
+      fontWeight: '900',
+      color: 'white',
+      textShadow: '2px 2px 6px #000',
+      transitionDuration: '1s'
+    },
+    ask:{
+      position: 'absolute',
+      bottom: '10%',
+      left: '5%',
+      transform: 'translateX(-10%)'
+    },
+    control:{
+      position: 'absolute',
+      bottom: '5%',
+      left: '5%',
+      content: 'url("./resource/forward_button.png")',
+      height: '30px',
+      width: '50px',
+      cursor: 'pointer'
+    } 
   }
 }
 
